07bfcf74b4c54b71b03cd13da98813d4f39dee97
Abhishek gour <gourabhishek026@gmail.com>
2023-07-10 22:07:15 +0530

    js variable and datatype is done

---
+++ 01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 100345;//not changeble
+let accountEmail = 'gourabhishek026@gmail.com';
+var accountPassword = '123456';
+let accountState = 'MP';
+
+/* All ways use let and const to declare a variable in javascript*/
+
+//accountId = 456;//not allowed
+
+accountEmail = 'xyz@gmail.com';
+accountPassword = '654321';
+accountState = 'Delhi';
+console.table([accountEmail,accountId,accountPassword,accountState]);
+
---
+++ 02_dataTypes.js
@@ -0,0 +1,31 @@
+/* alert(3+3) we are using nodejs, not browser*/
+
+// console.log(3 + 3);//allowed
+
+const name = "Abhishek gour";
+const age = 21;
+let state = 'MP';
+let isStudent = true;
+
+//number = 2 to power 53
+//bigint
+//string
+//boolean ==> true/false
+// null
+//undefined
+//symbol => unique
+
+// console.log(typeof age);
+// console.log(typeof null);
+
+
+/* Datatype conversion*/
+let isLoggedIN = 'Black Panther';
+let booleanIsLoggedIn = Boolean(isLoggedIN);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
