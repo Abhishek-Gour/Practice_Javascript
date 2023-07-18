6c576065b21d009dfdbb22ee8dd0986238aae195
Abhishek gour <gourabhishek026@gmail.com>
2023-07-18 09:02:56 +0530

    feat - a new feature is added like function and scopes in javascript

---
+++ 06_functionAndParameter.js
@@ -0,0 +1,54 @@
+function writeName() {
+    console.log("G");
+    console.log("o");
+    console.log("u");
+    console.log("r");
+}
+
+// writeName()
+
+// function addTwoNumbers(num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+// const result = addTwoNumbers(null, null);
+// console.log(result);//undefined;
+
+// function addTwoNumbers(num1, num2) {
+//     return num1 + num2;
+// }
+
+// let result = addTwoNumbers(1, 10);
+// console.log(result);
+
+
+//******************************** */
+
+
+// function loginUserMessage(userName = "Black panther") {
+//     if (userName === undefined) {
+//         console.log("Please enter a user name");
+//         return;
+//     }
+//     return `${userName} just logged in`;
+// }
+
+// console.log(loginUserMessage());
+
+
+//******************************** */
+
+// function calCulateCartPrice(num1 = 1, num2 = 2) {
+//     return num1 + num2;
+// }
+
+// console.log(calCulateCartPrice(10, 20));
+
+// function calCulateCartPrice(num1, num2 , ...num) {
+// return num1 + num2;
+// }
+
+// console.log(calCulateCartPrice(10, 20, 10,39,59)); o/p is 30
+
+
+
---
+++ 07_Scopes.js
@@ -0,0 +1,42 @@
+// var c = 300;
+// let a = 400; // global scope
+// if(true) {
+//     let a = 10;//block scope
+//     const b = 20;
+//     var c = 30;
+//     console.log(10); 
+// }
+
+// console.log(a) o/p 400;
+// console.log(b) o/p error
+// console.log(c) o/p 30
+
+// ********************************/
+
+// function one() {
+//     const userName = "Abhishek gour";
+
+//     function two() {
+//         userName = "Black Panter";
+//         console.log(userName);h
+//     }
+
+//     console.log(userName);
+//     two();
+// }
+
+// one();
+
+
+// if(true) {
+//     const userName = "Black Panter";
+//     if(userName === "Black Panter") {
+//         const programmerName = "Abhishek gour"
+//         console.log(userName +" "+ programmerName);
+//     }
+//     console.log(programmerName);
+// }
+
+// console.log(userName);
+
+
