56e147d94acfade24312305adc46429102bcfd69
Abhishek gour <gourabhishek026@gmail.com>
2023-07-24 21:52:19 +0530

    feat - a new feature is added like filter in js

---
+++ 11_filterMap.js
@@ -0,0 +1,30 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// const codeValues = coding.forEach((item) => {
+//     console.log(item);
+//    return item
+// })
+
+// console.log(codeValues);
+
+// const num = [1,2,3,4,5,6,7,8,9,10];
+
+
+// const numValues = num.filter((item) => item > 5)
+
+// console.log(numValues);
+
+// const num1 = [];
+
+// num.forEach((item) => {
+//     if(item > 5) {
+//         num1.push(item);
+//     }
+// })
+
+// console.log(num1);
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const num2 = num.filter((item) => item > 5);
+console.log(num2);
