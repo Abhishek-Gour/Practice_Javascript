7d31ac4936c8de3dceda0d038dcb4ec0c1ec59d4
Abhishek gour <gourabhishek026@gmail.com>
2023-07-24 22:11:56 +0530

    feat - a new feature is added like map and reduce in javascript

--- 11_filterMap.js
+++ 11_filterMap.js
@@ -1,3 +1,5 @@
+// filter in JavaScript
+
 // const coding = ["js", "ruby", "java", "python", "cpp"];
 
 // const codeValues = coding.forEach((item) => {
@@ -24,7 +26,40 @@
 
 // console.log(num1);
 
-const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const num2 = num.filter((item) => item > 5);
-console.log(num2);
+// const num2 = num.filter( function (item) {
+//     return item > 5;
+// });
+// console.log(num2);
+
+// ******************************
+
+// Map in JavaScript
+
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const numValues = num.map((item) => item + 10);
+
+// console.log(numValues);
+
+// const numValues = num.map((item) => item * 10).map((value) => value + 1).filter((value) => value > 10);
+
+// console.log(numValues);
+// **********************************
+
+
+// reduce in JavaScript
+
+const num = [1, 2, 3, 4, 5];
+
+
+// const numValues = num.reduce((acc, curValue) => { 
+//     console.log(`acc: ${acc} and curValue: ${curValue}`);
+//     return acc + curValue; }, 0)
+// console.log(numValues); o/p 15
+
+// const numValues = num.reduce((acc, curValue) => acc + curValue, 0);
+// console.log(numValues); o/p 15
+
+
